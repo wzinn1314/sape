5fcc4cf4f1259340d492661ca66dd38fb1aaf342
Wallisson <wallissonclipador@gmail.com>
2026-05-07 23:34:22 -0300

    mudei o nome da pasta

--- backend/main.sql
+++ backend/main.sql
@@ -4,6 +4,10 @@ USE sapedb;
 CREATE TABLE IF NOT EXISTS user (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(240) NOT NULL,
+<<<<<<< HEAD
     gmail VARCHAR(240) NOT NULL,
+=======
+    email VARCHAR(240) NOT NULL,
+>>>>>>> 146ac3d (banco de dados feito e funcionando, para colocar para rodar so da cd backend e depois npm run dev)
     password VARCHAR(240) NOT NULL
 );
